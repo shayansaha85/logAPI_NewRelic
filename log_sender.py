2a5f5c81f883a286964c4339207dad5d56b86e74
Shayan Saha <shayan851997@gmail.com>
2024-11-08 23:30:26 +0530

    configured continuous running

--- log_sender.py
+++ log_sender.py
@@ -8,6 +8,8 @@ config = configparser.ConfigParser()
 
 config.read('config.ini')
 
+interval = 30
+
 INSERT_API_KEY = config['NR']['insights_key']
 LOGS_API_URL = config['NR']['log_api_us']
 
@@ -23,51 +25,56 @@ env = config['SERVER']['env']
 
 local_file_path = './error.log'
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+def main():
+      ssh = paramiko.SSHClient()
+      ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-try:
-      ssh.connect(hostname, port, username, password)
-      stdin, stdout, stderr = ssh.exec_command(f'cat {remote_file_path}')
-      log_data = stdout.read().decode()
-      with open(local_file_path, 'w') as local_file:
-            local_file.write(log_data)
-      print(f"--- LOG FILE FETCHED TO LOCAL [{local_file_path}] ---")
+      try:
+            ssh.connect(hostname, port, username, password)
+            stdin, stdout, stderr = ssh.exec_command(f'cat {remote_file_path}')
+            log_data = stdout.read().decode()
+            with open(local_file_path, 'w') as local_file:
+                  local_file.write(log_data)
+            print(f"--- LOG FILE FETCHED TO LOCAL [{local_file_path}] ---")
 
-except Exception as e:
-      print(f"--- ERROR : {e} ---")
+      except Exception as e:
+            print(f"--- ERROR : {e} ---")
 
-finally:
-      ssh.close()
-
-
-logFile = open('error.log', 'r')
-logFileData = logFile.readlines()
-
-logFile.close()
-
-log_data = []
-o = []
-for data in logFileData:
-      if len(str(data).strip()) != 0:
-            log_data.append(
-                  {
-                        "timestamp": f"{int(time.time())}",
-                        "message": str(data).strip(),
-                        "log" : {
-                              "level" : log_level,
-                              "service": service,
-                              "environment": env,
-                              }
-                  }
-            )
+      finally:
+            ssh.close()
 
 
-headers = {
-    'Api-Key': INSERT_API_KEY,
-    'Content-Type': 'application/json',
-}
+      logFile = open('error.log', 'r')
+      logFileData = logFile.readlines()
 
-response = requests.post(LOGS_API_URL, headers=headers, data=json.dumps(log_data))
+      logFile.close()
 
-print(f"--- FILE SENT TO NEW RELIC | API STATUS CODE {response.status_code} ---")
+      log_data = []
+      o = []
+      for data in logFileData:
+            if len(str(data).strip()) != 0:
+                  log_data.append(
+                        {
+                              "timestamp": f"{int(time.time())}",
+                              "message": str(data).strip(),
+                              "log" : {
+                                    "level" : log_level,
+                                    "service": service,
+                                    "environment": env,
+                                    }
+                        }
+                  )
+
+
+      headers = {
+      'Api-Key': INSERT_API_KEY,
+      'Content-Type': 'application/json',
+      }
+
+      response = requests.post(LOGS_API_URL, headers=headers, data=json.dumps(log_data))
+
+      print(f"--- FILE SENT TO NEW RELIC | API STATUS CODE {response.status_code} ---")
+      
+while True:
+      main()
+      time.sleep(interval)
